04913160840e52087f8471a440e3437e8fd52cbb
Tobias Rehbein <tobias.rehbein@web.de>
2010-06-10 19:58:22 +0200

    Refactor ui_model handling in oggquiz.c
    `struct ui_model` was held in a static variable and was never freed. Resolve
    this issue by implementing dedicated functions to create and destroy the
    ui_model. Manage the lifetime of ui_model in main function.
    
    While here move the initial seeding of the random number generator to the main
    function eliminating the need for any static variables.
    
     * create dedicated functions to create and destroy `struct ui_model`
     * manage lifetime of `struct ui_model` in main function
     * seed random number generator in main function

--- oggquiz.c
+++ oggquiz.c
@@ -28,7 +28,9 @@ enum {
 };
 
 static void     avoid_zombie_processes(void);
-static int      new_turn(struct ogg_oggfile *oggfiles, struct plr_context *plr_ctx, struct ui_context *ui_ctx, struct opts_options *opts);
+struct ui_model *create_ui_model(struct opts_options *opts);
+void            destroy_ui_model(struct ui_model *model);
+static int      new_turn(struct ogg_oggfile *oggfiles, struct ui_model *model, struct plr_context *plr_ctx, struct ui_context *ui_ctx, struct opts_options *opts);
 
 int
 main(int argc, char **argv)
@@ -41,6 +43,7 @@ main(int argc, char **argv)
         struct ogg_context *ogg_ctx;
         struct plr_context *plr_ctx;
         struct ui_context *ui_ctx;
+        struct ui_model *ui_model;
 
         assert(argc >= 0);
         assert(argv != NULL);
@@ -54,16 +57,19 @@ main(int argc, char **argv)
          * only!
          */
 
+        if ((oggfiles = malloc(opts.choices * sizeof(struct ogg_oggfile))) == NULL)
+                err(EX_SOFTWARE, "could not malloc oggfiles");
         if ((ogg_ctx = ogg_context_open()) == NULL)
                 errx(EX_SOFTWARE, "could not open oggfile context");
         if ((plr_ctx = plr_context_open(opts.ogg123, opts.ogg123_options)) == NULL)
                 errx(EX_SOFTWARE, "could not open player context");
         if ((ui_ctx = ui_context_open()) == NULL)
                 errx(EX_SOFTWARE, "could not open ui context");
-        if ((oggfiles = malloc(opts.choices * sizeof(struct ogg_oggfile))) == NULL)
-                err(EX_SOFTWARE, "could not malloc oggfiles");
+        if ((ui_model = create_ui_model(&opts))== NULL)
+                err(EX_SOFTWARE, "could not create ui_model");
         avoid_zombie_processes();
 
+        srand(time(NULL));
         oggfileno = 0;
         while (fgets(filename, FILENAMELEN, stdin) != NULL) {
                 if ((newline = strchr(filename, '\n')) != NULL)
@@ -74,14 +80,15 @@ main(int argc, char **argv)
 
                 if (oggfileno == opts.choices) {
                         oggfileno = 0;
-                        if (new_turn(oggfiles, plr_ctx, ui_ctx, &opts))
+                        if (new_turn(oggfiles, ui_model, plr_ctx, ui_ctx, &opts))
                                 break;
                 }
         }
-
         plr_stop(plr_ctx);
-        plr_context_close(plr_ctx);
+
+        destroy_ui_model(ui_model);
         ui_context_close(ui_ctx);
+        plr_context_close(plr_ctx);
         if (ogg_context_close(ogg_ctx) != 0)
                 warn("could not close oggfile context");
         free(oggfiles);
@@ -90,10 +97,10 @@ main(int argc, char **argv)
 }
 
 static int
-new_turn(struct ogg_oggfile *oggfiles, struct plr_context *plr_ctx, struct ui_context *ui_ctx, struct opts_options *opts)
+new_turn(struct ogg_oggfile *oggfiles, struct ui_model *model,
+         struct plr_context *plr_ctx, struct ui_context *ui_ctx,
+         struct opts_options *opts)
 {
-        static int      first_invocation = 0;
-        static struct ui_model model;
         int             correct;
         int             i;
         char            guess;
@@ -102,30 +109,16 @@ new_turn(struct ogg_oggfile *oggfiles, struct plr_context *plr_ctx, struct ui_co
         assert(oggfiles != NULL);
         assert(opts != NULL);
 
-        if (!first_invocation) {
-                first_invocation = 1;
-                srand(time(NULL));
-                if ((model.scores = malloc(opts->players * sizeof(int))) == NULL)
-                        err(EX_SOFTWARE, "could not malloc scores");
-                if ((model.oggfiles = malloc(opts->choices * sizeof(struct ui_oggfile))) == NULL)
-                        err(EX_SOFTWARE, "could not malloc oggfiles");
-                for (i = 0; i < opts->players; i++) {
-                        model.scores[i] = 0;
-                }
-                model.turn = 0;
-                model.players = opts->players;
-                model.choices = opts->choices;
+        model->turn++;
+        model->current_player = ((model->turn - 1) % model->players);
+        for (i = 0; i < model->choices; i++) {
+                model->oggfiles[i].title = oggfiles[i].title;
+                model->oggfiles[i].artist = oggfiles[i].artist;
+                model->oggfiles[i].album = oggfiles[i].album;
         }
-        model.turn++;
-        model.current_player = ((model.turn - 1) % model.players);
-        for (i = 0; i < model.choices; i++) {
-                model.oggfiles[i].title = oggfiles[i].title;
-                model.oggfiles[i].artist = oggfiles[i].artist;
-                model.oggfiles[i].album = oggfiles[i].album;
-        }
-        correct = rand() % model.choices;
-        model.correct = &model.oggfiles[correct];
-        ui_display_quiz(ui_ctx, &model);
+        correct = rand() % model->choices;
+        model->correct = &model->oggfiles[correct];
+        ui_display_quiz(ui_ctx, model);
         plr_play(plr_ctx, oggfiles[correct].filename);
         start = time(NULL);
         do {
@@ -133,17 +126,51 @@ new_turn(struct ogg_oggfile *oggfiles, struct plr_context *plr_ctx, struct ui_co
                 if (guess == 'q')
                         return (1);
         } while (guess < '1' || guess > '0' + opts->choices);
-        model.guess = &model.oggfiles[guess - '1'];
-        if (model.guess == model.correct)
-                model.score_delta = MIN(opts->time, time(NULL) - start);
+        model->guess = &model->oggfiles[guess - '1'];
+        if (model->guess == model->correct)
+                model->score_delta = MIN(opts->time, time(NULL) - start);
         else
-                model.score_delta = opts->time;
-        model.scores[model.current_player] += model.score_delta;
-        ui_display_result(ui_ctx, &model);
+                model->score_delta = opts->time;
+        model->scores[model->current_player] += model->score_delta;
+        ui_display_result(ui_ctx, model);
         if (ui_get_key() == 'q')
                 return (1);
 
         return (0);
+}
+
+struct ui_model *
+create_ui_model(struct opts_options *opts)
+{
+        struct ui_model *model;
+        int             i;
+
+        assert(opts != NULL);
+
+        if ((model = malloc(sizeof(*model))) == NULL)
+                err(EX_SOFTWARE, "could not malloc ui_model");
+        if ((model->scores = malloc(opts->players * sizeof(int))) == NULL)
+                err(EX_SOFTWARE, "could not malloc scores");
+        if ((model->oggfiles = malloc(opts->choices * sizeof(struct ui_oggfile))) == NULL)
+                err(EX_SOFTWARE, "could not malloc oggfiles");
+        for (i = 0; i < opts->players; i++) {
+                model->scores[i] = 0;
+        }
+        model->turn = 0;
+        model->players = opts->players;
+        model->choices = opts->choices;
+
+        return (model);
+}
+
+void
+destroy_ui_model(struct ui_model *model)
+{
+        assert(model != NULL);
+
+        free(model->scores);
+        free(model->oggfiles);
+        free(model);
 }
 
 static void
